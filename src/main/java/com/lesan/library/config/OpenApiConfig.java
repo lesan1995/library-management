e7a170ad09fa9d90c56bb0e599c7a8d13fdf4c29
lesan1995 <lesan1995@gmail.com>
2026-05-15 08:02:29 +0700

    Add swagger

---
+++ src/main/java/com/lesan/library/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.lesan.library.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Library management API")
+                        .description("API Quản lý thư viện")
+                        .version("1.0")
+                        .contact(new io.swagger.v3.oas.models.info.Contact()
+                                .name("Tam")
+                                .email("lesan1995@gmail.com")));
+
+    }
+}
